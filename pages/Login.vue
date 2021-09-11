fe5b8f7bcbe367e1d906c8d3b0387b2d7076e657
claudionhangapc <claudionhangapc@gmail.com>
2021-09-11 09:55:58 -0300

    work on ohome page

--- pages/Login.vue
+++ pages/Login.vue
@@ -27,7 +27,7 @@
         <v-col cols="12"  class="pa-0 font-weight-black  mb-1">Senha</v-col>
         <v-col cols="12"  class="pa-0"> 
           <v-text-field
-          placeholder="E-mail"
+          placeholder="Senha"
           outlined
           dense
           full-width
